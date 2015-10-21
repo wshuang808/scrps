a88d56d2f169d0f79553e5017722043f37043c6c
wshuang <wshuang@office>
2015-10-21 10:40:03 +0800

    main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+var harvester = require('harvester');
+var builder = require('builder');
+var attacker = require('attack');
+for (var name in Game.creeps)
+{
+    var creep = Game.creeps[name];
+    if (creep.memory.role == 'harvester')
+    {
+        harvester(creep);
+    }
+    else if (creep.memory.role == 'builder')
+    {
+        builder(creep);
+    }
+    else if (creep.memory.role == 'guard')
+    {
+        attacker(creep);
+    }
+
+}
